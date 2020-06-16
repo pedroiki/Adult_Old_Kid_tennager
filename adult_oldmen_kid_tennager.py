8a15fd7f4c248573a13eff4356f938d082ef3ea7
Carlos Pedro Gomes <57367344+pedroiki@users.noreply.github.com>
2020-06-16 02:25:49 +0200

    Add files via upload

---
+++ adult_oldmen_kid_tennager.py
@@ -0,0 +1,10 @@
+
+age = int(input("Tell me your age: "))
+if idade >18 and idade <60:
+    print("adult")
+elif idade <18 and idade >12:
+    print("teenager")
+elif idade >60 :                    
+    print("Old")
+else:
+    print("kid")
